e8464a748eb17e46cca4a3d42c418150024617e7
RandaOsman <75427073+RandaOsman@users.noreply.github.com>
2020-12-25 14:02:37 +0200

    Update udac_example_dag.py

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -16,11 +16,30 @@ from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,LoadDim
 #     'start_date': datetime(2019, 1, 12),
 # }
 
+# dag = DAG('udac_example_dag',
+# #           default_args=default_args,
+#           description='Load and transform data in Redshift with Airflow',
+#           schedule_interval='0 * * * *',
+#           start_date=datetime.datetime(2019, 1, 12)
+#         )
+
+
+default_args = {
+    'owner': 'udacity',
+    'start_date': datetime.datetime(2019, 1, 12),
+    'depends_on_past': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'catchup': False,
+    'email_on_retry': False
+
+}
+
 dag = DAG('udac_example_dag',
-#           default_args=default_args,
+          default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
           schedule_interval='0 * * * *',
-          start_date=datetime.datetime(2019, 1, 12)
+          max_active_runs=1
         )
 
 start_operator = DummyOperator(
